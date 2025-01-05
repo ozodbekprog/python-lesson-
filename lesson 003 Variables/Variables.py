687394d457205b1ae6d6d349c66152f7b4ef2038
ozodbekprog <hoshimov281@gamil.com>
2025-01-05 21:42:16 +0500

    day 2 i learned varible

---
+++ lesson 003 Variables/Variables.py
@@ -0,0 +1,46 @@
+#03 dars
+# Author Ozodbek
+
+
+
+# ism ="Ozodbek"
+# yosh = 15
+# print(ism)
+# print(yosh)
+
+
+# ism = "Ozodbek"
+# print(ism)
+# ism = "Jasur"
+# print(ism)
+
+
+
+# radius = 5
+# pi = 3.14159
+# aylana_yuzi = pi * radius**2
+# print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+
+# home work
+# task 1
+# "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+
+
+# salom = ' Hello World!'
+# print(salom)
+
+# task 2
+# xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+
+# xabar = 'xabar'
+# print(xabar)
+# xabar = 'salom'
+# print(xabar)
+
+
+# task 3
+# class den nomlangan o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring (siz kutgan natija chiqdimi?)
+# Quyidagi kodni bajaring:
+# calass = ' salom '
+# print( salom)
