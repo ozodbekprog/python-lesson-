ecebd180ba0b17b5861c39cdaecb7457fb62cf60
ozodbekprog <hoshimov281@gamil.com>
2025-01-22 20:12:45 +0500

    string leson 005

---
+++ lesson 004 string/string.py
@@ -0,0 +1,85 @@
+# 05/01/2025
+# Lesson 4
+# Author Ozodbek
+
+# ism = 'ozodbek'
+
+
+# matin = 'i am happy 😂'
+# print(matin)
+# samyl = '👌'
+
+
+
+# ism = 'ozodbek'
+# print('meninig ismim ' + ism )
+# ism = ' ozodbek'
+# familiya ='hoshimov'
+# # print(ism +  ' '+ familiya)
+# ism_familiya = f"{ism} {familiya}"
+# print(ism_familiya)
+
+
+# ism = "ozodbek"
+# familiya = " hoshimov"
+# # print(f"salom, meninig ismim {familiya}. {ism} { familiya}")
+# print(f"ozobek hello are you good . {ism } { familiya}")
+
+
+
+# print("hello world!")
+# print("hello \t world!")
+# print("hello \n world!")
+
+# ism = "ozodbek"
+# familiya = " hoshimov"
+
+# isim_sharif = (f"{ism}{familiya}")
+# print(isim_sharif)
+
+# ism = "ozodbek"
+# familiya = "hoshimov"
+# print(f"salom meninig ism {familiya}.{ism} {familiya}")
+
+# print("hello world")
+# print("hello \tworld")
+# print("hello \nworld")
+
+# bosh modul
+# ism = "Tony"
+# familiya = "Harison"
+# ism_sharif = f"{ism} {familiya}"
+# ism_sharif = ism_sharif.lower()
+# print(f"Bosh holat: {ism_sharif} \nKeyingi natija:")
+
+
+# ism_sharif = ism_sharif.lower()
+# print(ism_sharif)
+# print(ism_sharif.upper())
+# print(ism_sharif.lower())
+# print(ism_sharif.title())
+
+
+# bosh harifni katalsh tirish
+# ism_sharif = ism_sharif.capitalize()
+# print(ism_sharif)
+
+#  upper metod hammasin kata qilish
+# ism_sharif = ism_sharif.upper()
+# print("upper() metodi ")
+# print(ism_sharif)
+
+
+meva ="      olma      "
+print(txt = "men " + meva.lstrip() + "yahshi korman.  lsrtip() metodi")
+print(txt2 = "men " + meva.rstrip() + "yahshi korman.  rstrip() metodi")
+print(txt3 = "men " + meva.strip() + "yahshi korman.   strip() metodi")
+print(txt4 = "men " + meva + "yashi korman.           no metod ")
+
+print(txt2.cout("yahshi"))
+
+# txt = "I love apples, apple are my favorite fruit"
+
+# x = txt.count("apple")
+
+# print(x)
